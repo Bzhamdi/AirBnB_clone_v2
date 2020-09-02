60dc3411bf84be0ba8755c2ae1ee889e64382efb
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-02 04:04:24 -0400

    use_param

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display C <text> """
-    return ("C %s" % text.replace('_', ' ')
+    return ("C %s" % text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
